d07fd59e3d7e443576b7be3589c7739999f85933
zaeim007 <alzaimahmed05@gmail.com>
2026-07-02 22:53:58 +0530

    Add Docker and Docker Compose support

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+
 from analyzer import analyze_log
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Upload folder configuration
+UPLOAD_FOLDER = "logs"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
@@ -13,16 +21,16 @@ def upload():
 
     file = request.files["logfile"]
 
-    filepath = f"logs/{file.filename}"
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
 
     file.save(filepath)
 
     result = analyze_log(filepath)
 
     return render_template(
-    "result.html",
-    data=result
-)
+        "result.html",
+        data=result
+    )
 
 
 if __name__ == "__main__":
